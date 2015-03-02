4300f6a238ceb90345933eb4690c71c56012a2b4
Daniel Moulton <danielzmoulton@gmail.com>
2015-03-02 10:01:36 -0500

    Added Files

---
+++ TestGame/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TestGame.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_TestGameTYPE                130
+#define ID_EDIT_REDO32771               32771
+#define ID_LEVEL_3COLORS                32772
+#define ID_LEVEL_4COLORS                32773
+#define ID_LEVEL_5COLORS                32774
+#define ID_Menu                         32775
+#define ID_LEVEL_7COLORS                32776
+#define ID_SETUP_BLOCKSIZE              32777
+#define ID_SETUP_BLOCKCOUNT             32778
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
